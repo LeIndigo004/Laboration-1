396fee2ba444d0e9fb310eafdc541be16bb95684
Leia Lindberg <ll224np@student.lnu.se>
2024-09-05 12:05:53 +0100

    Add style to form

--- src/js/index.js
+++ src/js/index.js
@@ -4,24 +4,3 @@
  * @author Leia Lindberg
  * @version 1.1.0
  */
-
-// Select the body element
-const bodyElement = document.querySelector('body')
-// Create a form element
-const form = document.createElement('form')
-
-// Create an input element for a name
-const inputName = document.createElement('input')
-// Set the type
-inputName.type = 'text'
-
-// Create a Header 1
-const headerText = document.createElement('h1')
-// Add the text
-headerText.innerHTML = 'Write your name here'
-
-// Append the input and header to the form
-form.appendChild(headerText)
-form.appendChild(inputName)
-// Append the form to the body
-bodyElement.appendChild(form)
