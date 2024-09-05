d7a33e2729d3f282b792dfb84e0a441b2d3898ab
Leia Lindberg <ll224np@student.lnu.se>
2024-09-05 16:18:48 +0100

    Display the name and flag of the country

--- src/js/index.js
+++ src/js/index.js
@@ -24,12 +24,43 @@ submitButton.addEventListener('click', (event) => {
 /**
  * A function for displaying the given country.
  */
-function displayCountry () {
-  fetchApi()
-  const form = document.querySelector('form')
-  const wrapper = document.querySelector('#wrapper')
-  form.style.display = 'none'
-  wrapper.style.backgroundColor = '#374375'
+async function displayCountry () {
+  try {
+    // Get the country from fetchApi function
+    const country = await fetchApi()
+
+    // Select the form
+    const form = document.querySelector('form')
+    // Select the wrapper
+    const wrapper = document.querySelector('#wrapper')
+
+    // Make form invisible
+    form.style.display = 'none'
+    wrapper.style.backgroundColor = '#374375'
+
+    // Create a new header
+    const h1 = document.createElement('h1')
+    // Put the countrys name as the header
+    h1.innerHTML = country.name.common
+    h1.setAttribute('class', 'countryName')
+    h1.style.color = '#FFFCF5'
+    wrapper.appendChild(h1)
+
+    // Get the flag image for the country
+    const flagImage = document.createElement('IMG')
+    flagImage.setAttribute('src', country.flags.png)
+    flagImage.setAttribute('alt', country.flags.alt)
+    wrapper.appendChild(flagImage)
+
+    // Add a smaller text as a welcome message
+    const name = document.querySelector('input')
+    const h3 = document.querySelector('h3')
+    h3.innerHTML = `Welcome ${name.value} to ${country.name.common}!`
+    h3.style.color = '#FFFCF5'
+    wrapper.appendChild(h3)
+  } catch (error) {
+    console.error(error.message)
+  }
 }
 
 /**
@@ -54,6 +85,8 @@ async function fetchApi () {
     const randomCountry = data[randomIndex]
     // log the country
     console.log(randomCountry)
+
+    return randomCountry
   } catch (error) {
     console.error(error.message)
   }
